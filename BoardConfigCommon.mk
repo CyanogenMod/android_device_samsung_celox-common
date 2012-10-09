727331200f33ab1c3668791bf692beb839abad29
David Ferguson <ferguson.david@gmail.com>
2012-10-09 17:14:39 -0400

    initial

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,47 @@
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from common msm8660
+-include device/samsung/msm8660-common/BoardConfigCommon.mk
+
+# Kernel
+BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true zcache
+BOARD_KERNEL_BASE           := 0x40400000
+BOARD_KERNEL_PAGESIZE       := 2048
+BOARD_FORCE_RAMDISK_ADDRESS := 0x41800000
+
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+# Suppress the WIPE command since it can brick our EMMC
+BOARD_SUPPRESS_EMMC_WIPE := true
+
+# Disable initlogo, Samsungs framebuffer is weird
+TARGET_NO_INITLOGO := true
+
+# Preload the boot animation to avoid jerkiness
+TARGET_BOOTANIMATION_PRELOAD := true
+
---
+++ celox-common.mk
@@ -0,0 +1,57 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+## The gps config appropriate for this device
+PRODUCT_COPY_FILES += device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
+
+## (2) Also get non-open-source specific aspects if available
+$(call inherit-product-if-exists, vendor/samsung/celox-common/celox-common-vendor.mk)
+
+## overlays
+DEVICE_PACKAGE_OVERLAYS += device/samsung/celox-common/overlay
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+# Hardware
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml 
+
+# Ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/celox-common/ramdisk/init.qcom.rc:root/init.qcom.rc \
+    device/samsung/celox-common/ramdisk/init.qcom.sh:root/init.qcom.sh \
+    device/samsung/celox-common/ramdisk/init.qcom.usb.sh:root/init.qcom.usb.sh \
+    device/samsung/celox-common/ramdisk/init.target.rc:root/init.target.rc \
+    device/samsung/celox-common/ramdisk/ueventd.rc:root/ueventd.rc \
+    device/samsung/celox-common/ramdisk/init.emmc.rc:root/init.emmc.rc \
+    device/samsung/celox-common/fstab.qcom:root/fstab.qcom
+
+# Vold
+PRODUCT_COPY_FILES += \
+    device/samsung/celox-common/vold.fstab:system/etc/vold.fstab
+
+# QRNGD
+PRODUCT_PACKAGES += qrngd
+
+# common msm8660
+$(call inherit-product, device/samsung/msm8660-common/msm8660.mk)
+
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
