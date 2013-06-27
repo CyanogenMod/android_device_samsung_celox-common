290beb3d9784afc71fc70d9a7f532f5293ebe6f5
Dave Carey <carvsdriver@gmail.com>
2013-06-27 10:24:46 -0700

    celox-common: use 4.7.3 toolchain
    Change-Id: Ifbc72c4abb6d5f96cd6b91aa120df35c667c921c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,8 +30,8 @@ BOARD_FAMILY := celox
 # inherit from common msm8660
 -include device/samsung/msm8660-common/BoardConfigCommon.mk
 
-# Kernel toolchain
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+# Kernel toolchain 		
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7.3
 
 # Kernel
 BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true zcache
