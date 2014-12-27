8145cbf62048078f324c4cc5dc5bff8ed67dee42
c457 <android.c357@gmail.com>
2014-12-27 05:32:27 -0600

    celox-common: Add exhilarate to the build barrier
    This device was left out and all nightlies have been broken
    
    Change-Id: I4a61ab79f0f962ccf5ece13ee14f568fdfa3df70

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter hercules skyrocket t769,$(TARGET_DEVICE)),)
+ifneq ($(filter exhilarate hercules skyrocket t769,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
